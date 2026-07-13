ba0cf61ebad06da8f299d9d5cb18122577f7f7a5
Madacascares21 <auxload.official@gmail.com>
2026-07-13 19:39:00 +0300

    new domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import netlify from '@netlify/vite-plugin-tanstack-start'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
+import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { generateSitemap } from 'tanstack-router-sitemap'
@@ -10,9 +10,11 @@ import { getSitemap } from '#/lib/sitemap'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(),   
-     generateSitemap(await getSitemap()),
-      tailwindcss(), tanstackStart(), viteReact(),    netlify(),],
+  plugins: [devtools(),
+  generateSitemap(await getSitemap()),
+  tailwindcss(), tanstackStart(), nitroV2Plugin({
+    preset: "node-server"
+  }), viteReact()],
 })
 
 export default config
