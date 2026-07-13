53983334752b1cc35d6f40112d87d14795747d28
Madacascares21 <auxload.official@gmail.com>
2026-07-13 22:36:21 +0300

    drizlle fxi

--- .output/server/chunks/_/kysely-migration-tables-JkVUjPF_-D0OvMFH3.mjs
+++
@@ -1,5 +0,0 @@
-var DEFAULT_MIGRATION_TABLE = "kysely_migration";
-var DEFAULT_MIGRATION_LOCK_TABLE = "kysely_migration_lock";
-
-export { DEFAULT_MIGRATION_TABLE as D, DEFAULT_MIGRATION_LOCK_TABLE as a };
-//# sourceMappingURL=kysely-migration-tables-JkVUjPF_-D0OvMFH3.mjs.map
