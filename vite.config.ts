ee582f466b67115984cff4e7b4345035bc6fbdf8
Madacascares21 <auxload.official@gmail.com>
2026-08-30 16:01:26 +0300

    dixx2

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
-import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { generateSitemap } from 'tanstack-router-sitemap'
-import { getSitemap } from '#/lib/sitemap'
+import { devtools } from '@tanstack/devtools-vite'
+import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [devtools(),
-  generateSitemap(await getSitemap()),
+
   tailwindcss(), tanstackStart(
     {
       sitemap: {
